951186e6171a2ff0d21db08189867ce471869ab6
AlulCode45 <alulgans70@gmail.com>
2025-09-27 10:56:27 +0700

    Update DeleteInterface to include UUID parameter in delete method

--- app/Base/Contracts/Interfaces/DeleteInterface.php
+++ app/Base/Contracts/Interfaces/DeleteInterface.php
@@ -3,8 +3,9 @@
 namespace App\Base\Contracts\Interfaces;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 interface DeleteInterface
 {
-    public function delete(): bool|array|Collection;
+    public function delete(string $uuid): bool|array|Collection|Model;
 }
