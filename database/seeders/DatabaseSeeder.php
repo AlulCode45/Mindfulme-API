d3aa192e307ad38982233428047df77560ac6f9a
AlulCode45 <alulgans70@gmail.com>
2025-10-10 10:07:25 +0700

    Refactor DatabaseSeeder to assign roles immediately after user creation for better clarity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,28 @@ class DatabaseSeeder extends Seeder
             Role::firstOrCreate(['name' => $role->value]);
         }
 
-        User::create([
+        $user = User::create([
             'name' => 'User',
             'email' => 'user@mail.com',
             'password' => bcrypt('password'),
         ]);
+        $user->assignRole(\App\Enums\Roles::USER->value);
+
+        // Super Admin
+        $superadmin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+        $superadmin->assignRole(\App\Enums\Roles::SUPERADMIN->value);
+
+        // Psychologist
+        $psychologist = User::create([
+            'name' => 'Psychologist',
+            'email' => 'psikolog@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+        $psychologist->assignRole(\App\Enums\Roles::PSYCHOLOGIST->value);
+
     }
 }
