560969efd628ea1ff3c8559ceff856afd07cca63
AlulCode45 <alulgans70@gmail.com>
2025-10-14 21:53:21 +0700

    Add role information to login response in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,6 +19,7 @@ class LoginController extends Controller
             return ResponseHelper::success([
                 'user' => $user,
                 'token' => $user->createToken('auth_token')->plainTextToken,
+                'role' => $user->getRoleNames()->first(),
             ], 'Login successful');
         }
 
